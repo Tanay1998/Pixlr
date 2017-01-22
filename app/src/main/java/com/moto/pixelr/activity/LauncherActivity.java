3b9dc56625bf27b7ba112dea3123e7f92e10cd59
huhx0015 <huhx0015@gmail.com>
2017-01-21 16:22:10 -0800

    Merged mdkutility into project.

--- app/src/main/java/com/moto/pixelr/activity/LauncherActivity.java
+++ app/src/main/java/com/moto/pixelr/activity/LauncherActivity.java
@@ -1,20 +1,15 @@
 package com.moto.pixelr.activity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import com.moto.pixelr.MainActivity;
 import com.moto.pixelr.R;
 import com.motorola.mod.ModManager;
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import butterknife.Unbinder;
 
 /**
@@ -25,14 +20,7 @@ public class LauncherActivity extends AppCompatActivity {
 
     private Unbinder unbinder;
 
-    @BindView(R.id.launcher_button) Button retryButton;
-    @BindView(R.id.launcher_progressBar) ProgressBar progressBar;
     @BindView(R.id.launcher_text) TextView launcherText;
-
-    @OnClick(R.id.launcher_button)
-    public void launcherButton() {
-        isDeviceMotoModReady(this);
-    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,8 +28,7 @@ public class LauncherActivity extends AppCompatActivity {
         setContentView(R.layout.activity_launcher);
         unbinder = ButterKnife.bind(this);
 
-        initView();
-        isDeviceMotoModReady(this);
+        checkMotoModsCompatibility();
     }
 
     @Override
@@ -50,46 +37,18 @@ public class LauncherActivity extends AppCompatActivity {
         unbinder.unbind();
     }
 
-    private void initView() {
-        launcherText.setText("Checking device compatibility...");
-        progressBar.setVisibility(View.VISIBLE);
-    }
+    public void checkMotoModsCompatibility() {
 
-    public void isDeviceMotoModReady(Context context) {
-
-        int service = ModManager.isModServicesAvailable(context);
-        String deviceStatus;
-        switch (service) {
-            case ModManager.SUCCESS:
-                launchActivity();
-                return;
-            case ModManager.SERVICE_MISSING:
-                deviceStatus = context.getString(R.string.SERVICE_MISSING);
-                break;
-            case ModManager.SERVICE_UPDATING:
-                deviceStatus = context.getString(R.string.SERVICE_UPDATING);
-                break;
-            case ModManager.SERVICE_VERSION_UPDATE_REQUIRED:
-                deviceStatus = context.getString(R.string.SERVICE_VERSION_UPDATE_REQUIRED);
-                break;
-            case ModManager.SERVICE_DISABLED:
-                deviceStatus = context.getString(R.string.SERVICE_DISABLED);
-                break;
-            case ModManager.SERVICE_INVALID:
-                deviceStatus = context.getString(R.string.SERVICE_INVALID);
-                break;
-            default:
-                deviceStatus = context.getString(R.string.SERVICE_NOT_AVAILABLE);
-                break;
+        // TODO: Disable this if testing on a non-Motorola device.
+        if (ModManager.isModServicesAvailable(LauncherActivity.this) == ModManager.SUCCESS) {
+            launchActivity();
+        } else {
+            launcherText.setText("This device does not support Moto Mods.");
         }
-
-        launcherText.setText(deviceStatus);
-        progressBar.setVisibility(View.INVISIBLE);
-        retryButton.setVisibility(View.VISIBLE);
     }
 
     private void launchActivity() {
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, PixelrActivity.class);
         startActivity(intent);
     }
 }
